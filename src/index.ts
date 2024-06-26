e1e659e329090fd849f9eb875c79c8e2a2aa1a32
Vaibhav <vs630916@gmail.com>
2024-06-26 13:53:31 +0530

    feat: Add Google OAuth2 authentication for Gmail API

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
 import express from "express";
+import { google } from "googleapis";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = 5000;
+
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+app.get("/auth/google", (req, res) => {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: [
+      "https://www.googleapis.com/auth/gmail.readonly",
+      "https://www.googleapis.com/auth/gmail.send",
+    ],
+  });
+  res.redirect(authUrl);
+});
+
+app.get("/auth/google/callback", async (req, res) => {
+  const { code } = req.query;
+  const { tokens } = await oAuth2Client.getToken(code as string);
+  oAuth2Client.setCredentials(tokens);
+  res.send("Authentication successful");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World from ReachInbox");
